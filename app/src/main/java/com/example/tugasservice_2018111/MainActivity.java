31121325cdd2ed1d25f30bea184360e78803e520
rahmahnur <rahmahnurhidayanti16.com>
2022-12-05 21:16:43 +0700

    TugasService_2018111

---
+++ app/src/main/java/com/example/tugasservice_2018111/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tugasservice_2018111;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart, btnStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MusikService.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,MusikService.class));
+            }
+        });
+
+    }
+}
